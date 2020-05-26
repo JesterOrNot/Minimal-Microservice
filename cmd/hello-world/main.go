607eea3c42d6d74b6c4b218e7ebe25ad153ed53b
Sean Hellum <seanhellum45@gmail.com>
2020-05-26 02:02:23 +0000

    Make the microservice even simpler

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -4,28 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
-
-const defaultAddr = ":8080"
 
 // main starts an http server on the $PORT environment variable.
 func main() {
-	addr := defaultAddr
-	// $PORT environment variable is provided in the Kubernetes deployment.
-	if p := os.Getenv("PORT"); p != "" {
-		addr = ":" + p
-	}
-
-	log.Printf("server starting to listen on %s", addr)
+	log.Printf("server starting to listen on port 8080")
 	http.HandleFunc("/", home)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("server listen error: %+v", err)
-	}
+    http.ListenAndServe(":8080", nil)
 }
 
 // home logs the received request and returns a simple response.
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received request: %s %s", r.Method, r.URL.Path)
-	fmt.Fprintf(w, "Hello, world!")
+	fmt.Fprintf(w, "Hello, world From a Microservice!")
 }
