04d836ba9a3765d33c464d4b9d77b66374dd257f
Sean Hellum <seanhellum45@gmail.com>
2020-05-26 02:13:18 +0000

    add a post request handlere

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	log.Printf("server starting to listen on port 8080")
 	http.HandleFunc("/", home)
+	http.HandleFunc("/api/hello", hello)
     http.ListenAndServe(":8080", nil)
 }
 
@@ -17,4 +18,19 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received request: %s %s", r.Method, r.URL.Path)
 	fmt.Fprintf(w, "Hello, world From a Microservice!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        fmt.Fprintf(w, "invalid_http_method")
+        return
+	}
+    // Must call ParseForm() before working with data
+    r.ParseForm()
+    // Log all data. Form is a map[]
+    log.Println(r.Form)
+
+    // Print the data back. We can use Form.Get() or Form["name"][0]
+    fmt.Fprintf(w, "Hello " + r.Form.Get("name"))
 }
