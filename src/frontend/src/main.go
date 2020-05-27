f56b46af048d21ce314b21698e8caf1f417c5dd8
Sean Hellum <seanhellum45@gmail.com>
2020-05-27 18:56:16 +0000

    use gin-gonic

--- src/frontend/src/main.go
+++ src/frontend/src/main.go
@@ -1,30 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
-// main starts an http server on the $PORT environment variable.
 func main() {
-	log.Printf("server starting to listen on port 8080")
-	http.HandleFunc("/", index)
-    http.ListenAndServe(":8080", nil)
-}
-
-// home logs the received request and returns a simple response.
-func index(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-        errorHandler(w, r, http.StatusNotFound)
-        return
-    }
-	http.ServeFile(w, r, "./static/index.html")
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-    w.WriteHeader(status)
-    if status == http.StatusNotFound {
-        fmt.Fprint(w, "custom 404")
-    }
+	server := gin.Default()
+	server.StaticFile("/", "./static/index.html")
+	server.Run()
 }
