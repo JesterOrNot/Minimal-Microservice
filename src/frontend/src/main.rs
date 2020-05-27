7ead1515e306af274d67030c0ff7e7bcf412d9c8
Sean Hellum <seanhellum45@gmail.com>
2020-05-27 19:28:28 +0000

    initialize polyglot version

---
+++ src/frontend/src/main.rs
@@ -0,0 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::response::NamedFile;
+
+#[get("/")]
+fn index() -> Result<NamedFile> {
+    NamedFile::open("www/index.html")
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
