aa15551faef2999b11a1a35724afe05c73bfe2bb
Sean Hellum <seanhellum45@gmail.com>
2020-06-09 16:50:45 -0500

    Add a voting microservice

--- src/backend/src/main.go
+++ src/backend/src/main.go
@@ -4,18 +4,23 @@ import "github.com/gin-gonic/gin"
 
 
 // Form Data
-type Form struct {
+type GreetForm struct {
 	Name string `form:"name"`
+}
+
+type VoteForm struct {
+  Age int `form:"age"`
 }
 
 func main() {
 	server := gin.Default()
 	server.GET("/api/hello", hello)
+  server.GET("/api/vote", canVote)
 	server.Run(":8081")
 }
 
 func hello(c *gin.Context) {
-	var form Form
+	var form GreetForm
 
 	if c.ShouldBind(&form) == nil {
 		c.JSON(200, gin.H{
@@ -23,3 +28,24 @@ func hello(c *gin.Context) {
 		})
 	}
 }
+
+func canVote(c *gin.Context) {
+  var form VoteForm
+
+  if c.ShouldBind(&form) == nil {
+    if form.Age <= 0 {
+      c.JSON(200, gin.H{
+        "canVote": "Error: Invalid Age",
+      })
+    } else if form.Age > 18 {
+      c.JSON(200, gin.H{
+        "canVote": true,
+      })
+    } else {
+      c.JSON(200, gin.H{
+        "canVote": false,
+      })
+    }
+  }
+}
+
--- src/frontend/static/index.js
+++ src/frontend/static/index.js
@@ -1,16 +1,29 @@
-function getTextInput() {
-    element = document.getElementById("greetInput");
-    value = element.value
-    element.value = "";
-    return value;
+function getTextValue(id) {
+    return function () {
+        element = document.getElementById(id)
+        value = element.value
+        element.value = ""
+        return value
+    }
 }
 
 function greet() {
-    fetch(`/api/hello?name=${getTextInput()}`).then(response => {
+    fetch(`/api/hello?name=${getTextValue("greetInput")()}`).then(response => {
         return response.json();
     }).then(data => {
-        console.log(data);
         document.getElementById("text").innerText = data.message;
+    });
+    fetch(`/api/vote?age=${getTextValue("ageInput")()}`).then(response => {
+        return response.json();
+    }).then(data => {
+        oldText = document.getElementById("text").innerText
+        message = ""
+        if (typeof data.canVote !== "boolean") {
+            alert(data.canVote)
+        } else {
+            message = data.canVote ? "you can vote!" : "you can't vote!"
+        }
+        document.getElementById("text").innerText = oldText + " " + message;
     });
     return true;
 }
