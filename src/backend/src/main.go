62ce6c348ade54e4d36d438cab5f5acca9e03c77
Sean Hellum <seanhellum45@gmail.com>
2020-06-10 17:51:00 +0000

    Use GoKit for maintainability

--- src/backend/src/main.go
+++ src/backend/src/main.go
@@ -1,51 +1,80 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
 
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
 
-// Form Data
-type GreetForm struct {
-	Name string `form:"name"`
+// Define Microservice's JSON Request API
+type voteRequest struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
-type VoteForm struct {
-  Age int `form:"age"`
+// Define Microservice's JSON Response
+type voteResponse struct {
+	Message string `json:"message,omitempty"`
+	Error   string `json:"error,omitempty"`
 }
+
+// VoteService Exposes the CanVote function to transport layer
+type VoteService interface {
+	CanVote(string, int) (string, error)
+}
+
+// The actual service where we implement the interface
+type voteService struct{}
 
 func main() {
-	server := gin.Default()
-	server.GET("/api/hello", hello)
-  server.GET("/api/vote", canVote)
-	server.Run(":8081")
+	svc := voteService{}
+
+	votingHandler := httptransport.NewServer(
+		makeVotingEndpoint(svc),
+		decodeVoteRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/api/vote", votingHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func hello(c *gin.Context) {
-	var form GreetForm
+func (voteService) CanVote(name string, age int) (string, error) {
+	if name == "" || age <= 0 {
+		return "", errors.New("Invalid properties")
+	} else if age >= 18 {
+		return "Hello " + name + ", you can vote!", nil
+	}
+	return "Hello " + name + ", you can't vote!", nil
+}
 
-	if c.ShouldBind(&form) == nil {
-		c.JSON(200, gin.H{
-			"message": "Hello " + form.Name,
-		})
+// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
+func makeVotingEndpoint(svc VoteService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(voteRequest)
+		resp, err := svc.CanVote(req.Name, req.Age)
+		if err != nil {
+			return voteResponse{resp, err.Error()}, nil
+		}
+		return voteResponse{resp, ""}, nil
 	}
 }
 
-func canVote(c *gin.Context) {
-  var form VoteForm
-
-  if c.ShouldBind(&form) == nil {
-    if form.Age <= 0 {
-      c.JSON(200, gin.H{
-        "canVote": "Error: Invalid Age",
-      })
-    } else if form.Age > 18 {
-      c.JSON(200, gin.H{
-        "canVote": true,
-      })
-    } else {
-      c.JSON(200, gin.H{
-        "canVote": false,
-      })
-    }
-  }
+// Decode JSON
+func decodeVoteRequest(_ context.Context, httpRequest *http.Request) (interface{}, error) {
+	var request voteRequest
+	if err := json.NewDecoder(httpRequest.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
 }
 
+// Encode JSON
+func encodeResponse(_ context.Context, rw http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(rw).Encode(response)
+}
